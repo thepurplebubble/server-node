d2e4dd89f13220ddc1260b54cc7664126728be50
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2024-07-05 00:57:46 -0400

    Add functions to interact with redis

--- index.js
+++ index.js
@@ -29,3 +29,38 @@ express.post("/sync/messages", (req, res) => {
 express.listen(process.env["PORT"], () => {
   console.log(`Purple Bubble Server is now listening on port ${process.env["PORT"]}`);
 });
+
+function storeMessage(message, callback) {
+  const recipient = message.recipient;
+  const hashKey = message.hash;
+  const messageStr = JSON.stringify(message);
+  const recipientSetKey = `recipient:${recipient}`;
+  const expiryTime = 7 * 24 * 60 * 60;
+
+  redis.set(hashKey, messageStr);
+  redis.sadd(recipientSetKey, hashKey);
+  redis.expire(hashKey, expiryTime);
+  redis.expire(recipientSetKey, expiryTime);
+}
+
+function searchByRecipient(recipient) {
+  const recipientSetKey = `recipient:${recipient}`;
+
+  redis.smembers(recipientSetKey, (hashKeys) => {
+    const results = [];
+    hashKeys.forEach((hashKey) => {
+      redis.get(hashKey, (messageStr) => {
+        if (messageStr) results.push(JSON.parse(messageStr));
+      });
+    });
+
+    return results;
+  });
+}
+
+function searchByHash(hashKey) {
+  redis.get(hashKey, (messageStr) => {
+    if (messageStr) return JSON.parse(messageStr);
+    else return null;
+  });
+}
