5ff5e9883dcbbce2ddb167e2f5cac321c6a72746
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2024-07-05 00:39:23 -0400

    Add endpoint placeholders

--- index.js
+++ index.js
@@ -10,6 +10,22 @@ const redis = createClient({
 redis.on("error", err => console.error("Redis Client Error", err));
 await redis.connect();
 
+express.post("/fetch", (req, res) => {
+
+});
+
+express.post("/send", (req, res) => {
+  
+});
+
+express.post("/sync/servers", (req, res) => {
+  
+});
+
+express.post("/sync/messages", (req, res) => {
+  
+});
+
 express.listen(process.env["PORT"], () => {
   console.log(`Purple Bubble Server is now listening on port ${process.env["PORT"]}`);
 });
