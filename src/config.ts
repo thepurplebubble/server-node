4c5116e506cf750373dd3d6af47c9f23319a9a6e
Kaden Frisk <fufferpish@gmail.com>
2024-09-21 15:14:11 -0500

    added better env validation in separate file

---
+++ src/config.ts
@@ -0,0 +1,19 @@
+const requiredEnvVars = {
+    REDIS_URL: 'Port number for the server',
+    PORT: 'URL for the database connection',
+};
+
+function validateEnv() {
+    const missingVars = Object.keys(requiredEnvVars).filter(key => !process.env[key]);
+
+    if (missingVars.length > 0) {
+        throw new Error(`Missing required environment variables: ${missingVars.join(', ')}`);
+    }
+}
+
+validateEnv();
+
+module.exports = {
+    redisURL: process.env.REDIS_URL,
+    port: process.env.PORT,
+};
